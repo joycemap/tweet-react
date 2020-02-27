cbd73a4ac1384476f4aa1799899776910d3a978c
joycemap <57893220+joycemap@users.noreply.github.com>
2020-02-27 09:20:24 +0800

    Edited error for ES6 syntax

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,82 +1,72 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import tweets from 'tweets'
-console.log(tweets)
+import tweets from "tweets";
+console.log(tweets);
 
 class App extends React.Component {
   render() {
-      let tweetsItems = tweets.tweets.tweets.map((tweet => {
-                  return <Tweet tweet={tweet} />
-                });
+    let tweetsItems = this.props.tweets.tweets.map(tweet => {
+      return <Tweet tweet={tweet} />;
+    });
 
-    return (
-      <div>
-        {tweetsItems}
-      </div>
-    );
+    return <div>{tweetsItems}</div>;
   }
 }
 class Tweet extends React.Component {
   render() {
-      return (
-          <div>
-              <User user={this.props.tweet.user} />
-              <Entities entities={this.props.tweet.entities} />
-              
-          </div>
-      );
+    return (
+      <div>
+        <User user={this.props.tweet.user} />
+        <Entities entities={this.props.tweet.entities} />
+      </div>
+    );
   }
 }
 
 class User extends React.Component {
   render() {
-      return (
-          <div>
-              <a href={this.props.user.url}>{this.props.user.name}</a>
-          </div>
-      );
+    return (
+      <div>
+        <a href={this.props.user.url}>{this.props.user.name}</a>
+      </div>
+    );
   }
 }
 
 class Entities extends React.Component {
   render() {
-      let hashtags = this.props.entities.hashtags.map(hashtag => {
-          return <p># {hashtag}</p>
-      })
-      return (
-          <div>
-              <p>Urls</p>
-              <Urls urls={this.props.entities.urls} />
-              <p>Hashtags</p>
-              {hashtags}
-          </div>
-      );
+    let hashtags = this.props.entities.hashtags.map(hashtag => {
+      return <p># {hashtag}</p>;
+    });
+    return (
+      <div>
+        <p>Urls</p>
+        <Urls urls={this.props.entities.urls} />
+        <p>Hashtags</p>
+        {hashtags}
+      </div>
+    );
   }
 }
 
 class Urls extends React.Component {
   render() {
-      let urlItem = this.props.urls.map(url => {
-          return(
-              <div>
-                  <a href={url.url}>URL</a>
-                  <a href={url.expanded_url}>Expanded URL</a>
-                  <a href={url.display_url}>{url.display_url}</a>
-              </div>
-          )
-      })
+    let urlItem = this.props.urls.map(url => {
       return (
-          <div>
-              {urlItem}
-          </div>
+        <div>
+          <a href={url.url}>URL</a>
+          <a href={url.expanded_url}>Expanded URL</a>
+          <a href={url.display_url}>{url.display_url}</a>
+        </div>
       );
+    });
+    return <div>{urlItem}</div>;
   }
 }
 
-const element = document.getElementById('app');
+const element = document.getElementById("app");
 
-ReactDOM.render(<App />, element );//
+ReactDOM.render(<App />, element); //
 
 console.log("tweet react");
-
